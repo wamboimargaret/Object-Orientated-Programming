4a1adefa385d8827482933db352406bf5667b988
wamboimargaret <margarettwamboi@gmail.com>
2023-06-27 23:06:00 +0300

    question3

--- oop.js
+++ oop.js
@@ -111,3 +111,80 @@ class Recepie{
    console.log(receipe2.ingredients)
 
 
+
+// 3.**Wildlife Preservation:** You're a wildlife conservationist working on a
+// program to track different species in a national park. Each species has its own
+// characteristics and behaviors, such as its diet, typical lifespan, migration
+// patterns, etc. Some species might be predators, others prey. You'll need to
+// create classes to model `Species`, `Predator`, `Prey`, etc., and think about how
+// These classes might relate to each other through inheritance.
+
+
+
+
+class Species {
+  constructor(name, diet, lifespan) {
+    this.name = name;
+    this.diet = diet;
+    this.lifespan = lifespan;
+  }
+ 
+ 
+  endangered_species() {
+  if (this.lifespan <= 50) {
+      return "Species is endangered";
+    } else {
+      return "Species is extinct";
+    }
+  }
+ }
+ 
+ 
+ class Predator extends Species {
+  constructor(name, diet, lifespan, hunting_method) {
+    super(name, diet, lifespan);
+    this.hunting_method = hunting_method;
+  }
+ 
+ 
+  dangerous() {
+    if (["stalk", "pounce", "chase"].includes(this.hunting_method)) {
+      return `Predator is carnivore and feeds on ${this.diet}`;
+    } else {
+      return `Predator is herbivore and feeds on ${this.diet}`;
+    }
+  }
+ }
+ 
+ 
+ class Prey extends Species {
+  constructor(name, diet, lifespan) {
+    super(name, diet, lifespan);
+  }
+ 
+ 
+  migration(need) {
+    if (need === "food") {
+      return `${this.name} migration is seasonal`;
+    } else if (need === "reproduction") {
+      return `${this.name} is migrating in July`;
+    }
+  }
+ }
+ 
+ 
+ let lion = new Predator("Lion", "meat", 15, "chase");
+ console.log(lion.dangerous());
+ 
+ 
+ let gazelle = new Prey("Gazelle", "grass", 10);
+ console.log(gazelle.migration("food"));
+ 
+ 
+ let elephant = new Species("Elephant", "plants", 70);
+ console.log(elephant.endangered_species());
+ 
+ 
+ elephant = new Prey("Elephant", "plants", 70);
+ console.log(elephant.migration("reproduction"));
+ 
